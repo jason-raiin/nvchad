d1e7e27a5af857e3c1208a325a0ee5082cbb3059
Jason <jiacan.h01@gmail.com>
2024-04-10 17:01:53 -0400

    Reorganise NvChad files

---
+++ lua/plugins/nvchad.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "NvChad/NvChad",
+    lazy = false,
+    branch = "v2.5",
+    import = "nvchad.plugins",
+    config = function()
+      require "configs.nvchad"
+    end,
+  },
+}
