7aa0a25cff3e5f8af5f6142ae47f4b966a3c98b6
Jason <jiacan.h01@gmail.com>
2024-04-12 11:57:17 -0400

    Map window navigation commands in terminal mode

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,4 +7,10 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+map("t", "<C-i>", "<C-\\><C-N>")
+map("t", "<C-h>", "<C-\\><C-N><C-w>h")
+map("t", "<C-j>", "<C-\\><C-N><C-w>j")
+map("t", "<C-k>", "<C-\\><C-N><C-w>k")
+map("t", "<C-l>", "<C-\\><C-N><C-w>l")
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
