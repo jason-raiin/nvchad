0d074ea42cd18414da080678c57b29633d55e655
Jason <jiacan.h01@gmail.com>
2024-04-18 16:47:17 -0400

    Add tailwind cmp colorizer

---
+++ lua/plugins/css-colorizer.lua
@@ -0,0 +1,16 @@
+return {
+  "roobert/tailwindcss-colorizer-cmp.nvim",
+  dependencies = {
+    "hrsh7th/nvim-cmp",
+    "NvChad/nvim-colorizer.lua"
+  },
+  -- optionally, override the default options:
+  config = function()
+    require("tailwindcss-colorizer-cmp").setup {
+      color_square_width = 2,
+    }
+    require("cmp").config.formatting = {
+      format = require("tailwindcss-colorizer-cmp").formatter,
+    }
+  end,
+}
---
+++ lua/plugins/nvim-colorizer.lua
@@ -0,0 +1,9 @@
+return {
+  "NvChad/nvim-colorizer.lua",
+  opts = {
+    css = true,
+    css_fn = true,
+    rgb_fn = true,
+    tailwind = true
+  }
+}
