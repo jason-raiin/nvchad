162a502090de1f79fa6518e03bd59a23001b7935
Jason <jiacan.h01@gmail.com>
2024-04-19 09:49:12 -0400

    Fix tailwind colorizer cmp config

---
+++ lua/configs/tailwindcss-colorizer-cmp.lua
@@ -0,0 +1,5 @@
+require("cmp").setup {
+  formatting = {
+    format = require("tailwindcss-colorizer-cmp").formatter,
+  },
+}
---
+++ lua/plugins/tailwindcss-colorizer-cmp.lua
@@ -0,0 +1,9 @@
+return {
+  "roobert/tailwindcss-colorizer-cmp.nvim",
+  dependencies = {
+    "hrsh7th/nvim-cmp",
+  },
+  opts = {
+    color_square_width = 2,
+  },
+}
