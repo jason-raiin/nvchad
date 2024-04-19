8e7cd4e87fbc8cc58c9d6d2b7364575d6604d2c0
Jason <jiacan.h01@gmail.com>
2024-04-19 11:18:55 -0400

    Configure nvim-notify to have compact notifications and enable Telescope history

--- lua/plugins/nvim-notify.lua
+++ lua/plugins/nvim-notify.lua
@@ -1,7 +1,15 @@
 return {
   "rcarriga/nvim-notify",
+  dependencies = { "nvim-telescope/telescope.nvim" },
   opts = {
     -- other stuff
     background_colour = "#000000",
+    fps = 60,
+    max_width = 100,
+    max_height = 40,
+    render = "wrapped-compact",
   },
+  configs = function()
+    require("telescope").load_extension "notify"
+  end,
 }
