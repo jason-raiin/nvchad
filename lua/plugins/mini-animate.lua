dee79509d933cd387b0bf53bff5ccb277c5e94f9
Jason <jiacan.h01@gmail.com>
2024-04-19 12:45:20 -0400

    Remove window open and close animations, configure other animations to be smoother

--- lua/plugins/mini-animate.lua
+++ lua/plugins/mini-animate.lua
@@ -1,6 +1,44 @@
 return {
   "echasnovski/mini.animate",
   init = function()
-    require("mini.animate").setup()
+    -- check for mouse scroll action
+    local mouse_scrolled = false
+    for _, scroll in ipairs { "Up", "Down" } do
+      local key = "<ScrollWheel" .. scroll .. ">"
+      vim.keymap.set({ "", "i" }, key, function()
+        mouse_scrolled = true
+        return key
+      end, { expr = true })
+    end
+
+    local animate = require "mini.animate"
+    animate.setup {
+      resize = {
+        timing = animate.gen_timing.linear { duration = 50, unit = "total" },
+      },
+      scroll = {
+        timing = animate.gen_timing.linear { duration = 100, unit = "total" },
+        -- do not animate for mouse scrolls
+        subscroll = animate.gen_subscroll.equal {
+          predicate = function(total_scroll)
+            if mouse_scrolled then
+              mouse_scrolled = false
+              return false
+            end
+            return total_scroll > 1
+          end,
+        },
+      },
+      open = {
+        enable = false,
+        winconfig = animate.gen_winconfig.wipe { direction = "from_edge" },
+        winblend = animate.gen_winblend.linear { from = 80, to = 100 },
+      },
+      close = {
+        enable = false,
+        winconfig = animate.gen_winconfig.wipe { direction = "to_edge" },
+        winblend = animate.gen_winblend.linear { from = 100, to = 80 },
+      },
+    }
   end,
 }
