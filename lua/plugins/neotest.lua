73681b9308854ff633273b4ef8847779fba60fe6
Jason <jiacan.h01@gmail.com>
2024-04-30 14:33:21 -0400

    Add neotest plugin

---
+++ lua/plugins/neotest.lua
@@ -0,0 +1,31 @@
+return {
+  "nvim-neotest/neotest",
+  dependencies = {
+    "nvim-neotest/nvim-nio",
+    "nvim-lua/plenary.nvim",
+    "antoinemadec/FixCursorHold.nvim",
+    "nvim-treesitter/nvim-treesitter",
+    "rcasia/neotest-java",
+    "nvim-neotest/neotest-jest",
+    "nvim-neotest/neotest-plenary"
+  },
+  config = function()
+    require('neotest').setup {
+      adapters = {
+        require("neotest-java"),
+        require("neotest-jest"),
+        require("neotest-plenary")
+      }
+    }
+  end,
+  keys = {
+    { "<leader>tt", function() require("neotest").run.run(vim.fn.expand("%")) end, desc = "Run File" },
+    { "<leader>tT", function() require("neotest").run.run(vim.uv.cwd()) end, desc = "Run All Test Files" },
+    { "<leader>tr", function() require("neotest").run.run() end, desc = "Run Nearest" },
+    { "<leader>tl", function() require("neotest").run.run_last() end, desc = "Run Last" },
+    { "<leader>ts", function() require("neotest").summary.toggle() end, desc = "Toggle Summary" },
+    { "<leader>to", function() require("neotest").output.open({ enter = true, auto_close = true }) end, desc = "Show Output" },
+    { "<leader>tO", function() require("neotest").output_panel.toggle() end, desc = "Toggle Output Panel" },
+    { "<leader>tS", function() require("neotest").run.stop() end, desc = "Stop" },
+  }
+}
