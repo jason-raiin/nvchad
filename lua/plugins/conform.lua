92640016ec491d795fad47a1b476568c17a1d097
Jason <jiacan.h01@gmail.com>
2024-04-30 20:06:25 -0400

    Add conform-mason for automatic installation

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,13 +1,25 @@
 return {
-  "stevearc/conform.nvim",
-  cmd = { "ConformInfo" },
-  opts = {
-    formatters_by_ft = {
-      lua = { "stylua" },
-      html = { "prettierd" },
-      css = { "prettierd" },
-      typescript = { "eslint_d", "prettierd" },
-      graphql = { "prettierd" }
+  {
+    "stevearc/conform.nvim",
+    cmd = { "ConformInfo" },
+    opts = {
+      formatters_by_ft = {
+        lua = { "stylua" },
+        html = { "prettierd" },
+        css = { "prettierd" },
+        typescript = { { "eslint_d", "prettierd" } },
+        graphql = { "prettierd" }
+      }
     }
+  },
+  {
+    "zapling/mason-conform.nvim",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "stevearc/conform.nvim"
+    },
+    init = function()
+      require "mason-conform".setup()
+    end
   }
 }
