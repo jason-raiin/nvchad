960d99695961118cf24e9d6fef5bf8bd278165f6
Jason <jiacan.h01@gmail.com>
2024-05-09 16:30:45 -0400

    Add telescope-ui-select plugin for code actions

---
+++ lua/plugins/ui-select.lua
@@ -0,0 +1,15 @@
+return {
+  "nvim-telescope/telescope-ui-select.nvim",
+  dependencies = "nvim-telescope/telescope.nvim",
+  init = function()
+    local telescope = require "telescope"
+    telescope.setup {
+      extensions = {
+        ["ui-select"] = {
+          require("telescope.themes").get_cursor(),
+        },
+      },
+    }
+    telescope.load_extension "ui-select"
+  end,
+}
