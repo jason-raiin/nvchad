248c76c2e3bb33cd87eb1fdf09cb8a97ae2ca78f
Jason <jiacan.h01@gmail.com>
2024-05-10 13:38:03 -0400

    Fix key mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -4,13 +4,25 @@ require "nvchad.mappings"
 
 local map = vim.keymap.set
 
-map("n", ";", ":", { desc = "CMD enter command mode" })
+-- vim QOL
+map("n", ";", ":")
 map("i", "jk", "<ESC>")
 
+-- exit terminal mode with Option+q
 map("t", "<M-q>", "<C-\\><C-N>")
+
+-- navigate windows in terminal mode
 map("t", "<C-h>", "<C-\\><C-N><C-w>h")
 map("t", "<C-j>", "<C-\\><C-N><C-w>j")
 map("t", "<C-k>", "<C-\\><C-N><C-w>k")
 map("t", "<C-l>", "<C-\\><C-N><C-w>l")
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+-- close windows
+map("n", "<M-w>", "<C-w>q")
+
+-- linebreak at cursor
+map("n", "<C-cr>", "<esc>i<cr><esc>")
+map("n", "<C-bs>", "<esc>0i <esc>0cw<bs> <esc>")
+
+-- Ctrl-s to save
+map("i", "<C-s>", "<cmd>w<cr>")
--- lua/plugins/mini-move.lua
+++ lua/plugins/mini-move.lua
@@ -1,9 +1,7 @@
 return {
   "echasnovski/mini.move",
+  event = "BufEnter",
   config = function()
     require("mini.move").setup()
   end,
-  keys = {
-    "<M-h>", "<M-j>", "<M-k>", "<M-l>"
-  }
 }
