310fe9222737b6b4dc60b77a336b58edd795bd27
Jason <jiacan.h01@gmail.com>
2024-05-24 16:36:26 -0400

    Add nvim undotree plugin

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -1,6 +1,27 @@
 return {
-  "mbbill/undotree",
-  enabled = false,
-  cmd = { "UndotreeToggle" },
-  keys = { { "<leader>u", "<cmd>UndotreeToggle<cr>", desc = "Toggle Undotree" } },
+  "jiaoshijie/undotree",
+  dependencies = "nvim-lua/plenary.nvim",
+  keys = { -- load the plugin only when using it's keybinding:
+    {
+      "<leader>u",
+      function()
+        require("undotree").toggle()
+        vim.cmd "vertical resize 40"
+      end,
+    },
+  },
+  opts = {
+    keymaps = {
+      ["<Down>"] = "move_next",
+      ["<Up>"] = "move_prev",
+      ["<Left>"] = "move2parent",
+      ["<M-Down>"] = "move_change_next",
+      ["<M-Up>"] = "move_change_prev",
+      ["<Right>"] = "enter_diffbuf",
+    },
+    window = {
+      height = 30,
+      width = 30,
+    },
+  },
 }
