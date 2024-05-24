1445efcdadc1f551a4ff8808f3d6b5faf69b895b
Jason <jiacan.h01@gmail.com>
2024-05-24 11:05:41 -0400

    Add trouble for diagnostics list

---
+++ lua/plugins/trouble.lua
@@ -0,0 +1,8 @@
+return {
+  "folke/trouble.nvim",
+  branch = "dev",
+  keys = {
+    { "<leader>dd", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Buffer Diagnostics (Trouble)" }
+  },
+  opts = {},
+}
