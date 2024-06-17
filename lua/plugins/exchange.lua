a63e17c5efc33c72430cd6efe21cb2b2da4229bc
Jason <jiacan.h01@gmail.com>
2024-06-17 13:18:50 -0400

    Add exchange plugin

---
+++ lua/plugins/exchange.lua
@@ -0,0 +1,9 @@
+return {
+  "tommcdo/vim-exchange",
+  keys = {
+    { "cx", mode = "n", desc = "exchange selector" },
+    { "cxx", mode = "n", desc = "exchange line" },
+    { "X", mode = "x", desc = "exchange visual" },
+    { "cxc", mode = "n", desc = "clear exchange" },
+  },
+}
