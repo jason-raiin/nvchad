7a6ebe365c9a108386608dac1deb4d6b8fb3798c
Jason <jiacan.h01@gmail.com>
2024-09-23 10:23:23 -0400

    Replace copilot with Supermaven

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -1,21 +1,9 @@
 return {
   {
-    "github/copilot.vim",
-    event = "VeryLazy"
-  },
-  {
-    "CopilotC-Nvim/CopilotChat.nvim",
-    branch = "canary",
-    dependencies = {
-      { "github/copilot.vim" }, -- or github/copilot.vim
-      { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+    {
+      "supermaven-inc/supermaven-nvim",
+      opts = {},
+      event = "VeryLazy",
     },
-    build = "make tiktoken", -- Only on MacOS or Linux
-    opts = {
-      debug = true, -- Enable debugging
-      -- See Configuration section for rest
-    },
-    -- See Commands section for default commands if you want to lazy load on them
-    cmd = "CopilotChat",
   },
 }
